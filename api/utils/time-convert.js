9f8efd6584f6c5280ae8caceb29b0b0f7ced6d79
hoang <bdhoang.ht@gmail.com>
2018-07-24 00:17:50 +0700

    update

--- api/utils/time-convert.js
+++ api/utils/time-convert.js
@@ -2,7 +2,7 @@ let moment = require('moment-timezone');
 module.exports = function (objectArray) {
     return new Promise(function (resolve) {
         objectArray.forEach(object => {
-            object.time = moment(object.time.getTime()).tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss.SSSS');
+            object.time = moment(object.time.getTime()).tz('Asia/Ho_Chi_Minh').format('YYYY/MM/DD HH:mm:ss.SSSS');
         });
         resolve(objectArray);
     });
