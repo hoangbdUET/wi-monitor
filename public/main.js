e4b2ec5862438ebca5a3dcbba519f481ce3d59d0
hoang <bdhoang.ht@gmail.com>
2018-07-25 10:57:18 +0700

    hihihihhi

--- public/main.js
+++ public/main.js
@@ -17,15 +17,36 @@ app.controller('appController', async function ($scope, $http) {
             { data: 'duration' },
         ],
     });
+
+    const mdt = $('#mean-table').DataTable({
+        fixedHeader: true,
+        columns: [
+            { data: 'index' },
+            { data: 'username' },
+            { data: 'path' },
+            { data: 'time' },
+            { data: 'duration' },
+        ],
+    });
     $scope.days = '1';
+    $scope.hours = '1';
     async function getRequests () {
-        const allRes = await $http.get(`${host}/response/all?days=${$scope.days}`);
+        const allRes = await $http.get(`${host}/response/all?hours=${$scope.hours}`);
         $scope.all = allRes.data.map((item, index) => Object.assign(item, { index: index + 1 }));
         dt.clear();
         dt.rows.add($scope.all);
         dt.draw();
     }
+    async function getMeanRequests () {
+        const allRes = await $http.get(`${host}/response/mean/all?days=${$scope.days}`);
+        $scope.all = allRes.data.map((item, index) => Object.assign(item, { index: index + 1 }));
+        mdt.clear();
+        mdt.rows.add($scope.all);
+        mdt.draw();
+    }
     $scope.getRequests = getRequests;
+    $scope.getMeanRequests = getMeanRequests;
     getRequests();
+    getMeanRequests();
     $scope.$apply();
 });
