91305c66561083d3d3fcec06255d8e12e0ec7eb6
BuiHoang <bdhoang.ht@gmail.com>
2018-11-21 10:44:38 +0700

    fix

--- api/response_times.js
+++ api/response_times.js
@@ -5,44 +5,44 @@ const convertTime = require('./utils/time-convert');
 let router = express.Router();
 
 router.get('/response/count', function (req, res) {
-	influx.query('select count(*) from response_times').then(result => {
-		// console.log(result);
-		res.json(result);
-	}).catch(err => {
-		res.send(err);
-	})
+    influx.query('select count(*) from response_times').then(result => {
+        // console.log(result);
+        res.json(result);
+    }).catch(err => {
+        res.send(err);
+    })
 });
 
 router.get('/response/all', function (req, res) {
-	let whereClause = "";
-	let hours = req.query.hours || 1;
-	if (req.query.username) whereClause += " AND username = '" + req.query.username + "'";
-	whereClause += " AND time > now() - " + hours + "h";
-	let query = "SELECT * FROM response_times WHERE 1=1 " + whereClause;
-	console.log(query);
-	influx.query(query).then(result => {
-		convertTime(result).then(r => {
-			res.send(r);
-		})
-	}).catch(err => {
-		res.send(err);
-	})
+    let whereClause = "";
+    let hours = req.query.hours || 1;
+    if (req.query.username) whereClause += " AND username = '" + req.query.username + "'";
+    whereClause += " AND time > now() - " + hours + "h";
+    let query = "SELECT * FROM response_times WHERE 1=1 " + whereClause + " ORDER BY time DESC";
+    console.log(query);
+    influx.query(query).then(result => {
+        convertTime(result).then(r => {
+            res.send(r);
+        })
+    }).catch(err => {
+        res.send(err);
+    })
 });
 
-router.get('/response/mean/all', (req, res)=>{
-	let whereClause = "";
-	let days = req.query.days || 1;
-	if (req.query.username) whereClause += " AND username = '" + req.query.username + "'";
-	whereClause += " AND time > now() - " + days + "d";
-	let query = "SELECT * FROM three_months.mean_response_times_2 WHERE 1=1 " + whereClause;
-	console.log(query);
-	influx.query(query).then(result => {
-		convertTime(result).then(r => {
-			res.send(r);
-		})
-	}).catch(err => {
-		res.send(err);
-	});
+router.get('/response/mean/all', (req, res) => {
+    let whereClause = "";
+    let days = req.query.days || 1;
+    if (req.query.username) whereClause += " AND username = '" + req.query.username + "'";
+    whereClause += " AND time > now() - " + days + "d";
+    let query = "SELECT * FROM three_months.mean_response_times_2 WHERE 1=1 " + whereClause + " ORDER BY time DESC";
+    console.log(query);
+    influx.query(query).then(result => {
+        convertTime(result).then(r => {
+            res.send(r);
+        })
+    }).catch(err => {
+        res.send(err);
+    });
 });
 
 module.exports = router;
